705bc6d71b6c0d785eadeeb3d828541d23164cf3
Abdellah <abdellahhlali@gmail.com>
2025-05-08 15:26:06 +0200

    Premiere version de Abdel

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,41 @@ const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mysql = require('mysql2');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [];
+// Connexion à MySQL
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'tonMDP',
+  database: 'projet_bda'
+});
 
+db.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err.message);
+  } else {
+    console.log('Connecté à la base de données MySQL');
+  }
+});
+
+// Route de base
 app.get('/', (req, res) => {
   res.json({ success: true, message: 'Bienvenue sur l’API' });
+});
+
+
+
+// Exemple de route
+app.get('/user', (req, res) => {
+  db.query('SELECT * FROM user', (err, results) => {
+    if (err) return res.status(500).json({ erreur: err.message });
+    res.json(results);
+  });
 });
 
 app.listen(3000, () => {
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router' // 👈
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router) // 👈 Active le router
+app.mount('#app')
